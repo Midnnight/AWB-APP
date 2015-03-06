70d5882103ac5ee2b2e74fed6d2a555a499cb6c2
Lasri Reda <lasri1reda@gmail.com>
2015-03-06 09:30:25 +0000

    swipe to send email

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/applications-search-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/applications-search-view.js
@@ -9,3 +9,17 @@ $('#list-results').children('li').on('click', function () {
 	property = applications[application_index].customer.property;
 	customer = applications[application_index].customer;
 });
+
+$('#list-results').children('li').on('swipeleft', function () {
+	var client =$(this);
+	client.animate({ left:'-20%' }, 500);
+	setTimeout(function(){
+		client.animate({ left:'0px' }, 500);
+	},3000);
+});
+
+$('#list-results').children('li').on('swiperight', function () {
+	var client =$(this);
+	client.animate({ left:'0px' }, 500);
+	
+});
