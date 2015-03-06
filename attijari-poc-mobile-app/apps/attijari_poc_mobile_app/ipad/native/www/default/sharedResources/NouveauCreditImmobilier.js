945da45d3d0562ca42ebfb3617f31b646a59b844
imadgit <ouchenimad@gmail.com>
2015-03-06 16:36:50 +0000

    save all parameters in jsonstore

---
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/ipad/native/www/default/sharedResources/NouveauCreditImmobilier.js
@@ -0,0 +1,35 @@
+
+/* JavaScript content from sharedResources/NouveauCreditImmobilier.js in folder common */
+var NouveauCreditImmobilier={
+		Infoclient:{
+			"PicUrl":"",
+			"Prenom":"",		"Nom":"",			"dob":"",		"adresse":"",		"ville":"",		"Nationality":"Maroc",
+			"LangPref":"Français",		"Mobile":"",		"Fixe":"",		"Email":"",		"Comm":"1",			"ComptC":"",
+			"ComptE":"",		"SalMen":"",		"AddIncome":"",		"IdDoc":"",		"DernierSal":""
+		},
+		DetailProperty:{
+			"adresse":"",		"Structure":"",		"ResComm":"",		"TypeImmeuble":"",		"ResPrimary":"",		"SurfaceTerre":"",
+			"SurfaceConstLog":"",		"Entrepreneurs":"",		"EstimationConstruct":"",		"EstimationProperty":""
+		},
+		DetailCredit:{
+			"DureePret":"",			"TauxInteret":"",		"MontantPret":"",
+			"DateOrg":"",			"Echeance":"",			"Mensualite":""
+		}
+};
+
+var AnnulerInit={
+		Infoclient:{
+			"PicUrl":"",
+			"Prenom":"",	"Nom":"",		"dob":"",		"adresse":"",		"ville":"",		"Nationality":"Maroc",		"LangPref":"Français",
+			"Mobile":"",	"Fixe":"",		"Email":"",		"Comm":"1",		"ComptC":"",		"ComptE":"",			"SalMen":"",
+			"AddIncome":"",		"IdDoc":"",		"DernierSal":""
+		},
+		DetailProperty:{
+			"adresse":"",		"Structure":"",		"ResComm":"",		"TypeImmeuble":"",		"ResPrimary":"",		"SurfaceTerre":"",
+			"SurfaceConstLog":"",		"Entrepreneurs":"",			"EstimationConstruct":"",			"EstimationProperty":""
+		},
+		DetailCredit:{
+			"DureePret":"",			"TauxInteret":"",		"MontantPret":"",		"DateOrg":"",
+			"Echeance":"",			"Mensualite":""
+		}
+};
---
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/ipad/native/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"date":1425652403112,"machine":"gara-PC","checksum":1961813034};
+/* Date: Fri Mar 06 14:33:23 WET 2015 */
