0ae3b056b9615bda035bbc84de1b7ced702bdcdf
imadgit <ouchenimad@gmail.com>
2015-03-10 11:46:49 +0000

    chat part2

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/ipad/native/www/default/worklight/checksum.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/ipad/native/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1425979849285,"machine":"gara-PC","checksum":550093955};
-/* Date: Tue Mar 10 09:30:49 WET 2015 */
+var WL_CHECKSUM = {"date":1425986912809,"machine":"gara-PC","checksum":3906739043};
+/* Date: Tue Mar 10 11:28:32 WET 2015 */
